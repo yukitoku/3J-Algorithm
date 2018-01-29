28b62ddff5719be9f08a79f6798b6406974a0ad9
yukitoku <yuki.toku@outlook.jp>
2018-01-29 23:09:11 +0900

    Update ruthaaron.c
    基数を10億にして高速化

--- ruthaaron.c
+++ ruthaaron.c
@@ -1,31 +1,17 @@
-#define KETA 6
+#define KETA 101
+#define KISU 1000000000 //基数が10億
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <limits.h>
-#include <string.h>
+#include <sys/time.h>
 
 struct NUMBER{
 	int n[KETA];//各桁の値
-	int sign;//符号変数
 };
 
-int setSign(struct NUMBER *a, int s){
-	if(s >= 0)
-		a->sign = 1;
-	else
-		a->sign = -1;
-}
-
-int getSign(struct NUMBER *a){
-	if(a->sign < 0)
-		return -1;
-	else
-		return 1;
-}
-
-void clearByZero(struct NUMBER *a){//ゼロクリア&符号を+にセット
+//ゼロクリア
+void clearByZero(struct NUMBER *a){
 	int i = 0;
 
 	while(1){
@@ -33,11 +19,10 @@ void clearByZero(struct NUMBER *a){//ゼロクリア&符号を+にセット
 		a->n[i] = 0;
 		i++;
 	}
-	setSign(a,0);
-	//a->sign = 1;//符号をプラスにセット
 }
 
-void dispNumber(struct NUMBER *a){//表示関数
+//表示関数
+void dispNumber(struct NUMBER *a){
 	int i = KETA - 1;
 		printf("+ ");
 	while(1){
@@ -47,20 +32,20 @@ void dispNumber(struct NUMBER *a){//表示関数
 	}
 }
 
-void setRnd(struct NUMBER *a,int k){//乱数格納
+//乱数格納
+void setRnd(struct NUMBER *a,int k){
 	int i = 0;
-	a->sign = 1;
 
 	while(1){
 		if(i > k - 1) break;
-		a->n[i] = random() % 10;
+		a->n[i] = random() % KISU;
 		i++;
 	}
 }
 
-void copyNumber(struct NUMBER *a, struct NUMBER *b){//aをbにコピー
+//aをbにコピー
+void copyNumber(struct NUMBER *a, struct NUMBER *b){
 	int i = 0;
-	b->sign = a->sign;
 
 	 while(1){
 	 	if(i > KETA - 1) break;
@@ -69,7 +54,8 @@ void copyNumber(struct NUMBER *a, struct NUMBER *b){//aをbにコピー
 	 } 
 }
 
-int isZero(struct NUMBER *a){//ゼロかどうか
+//ゼロかどうか
+int isZero(struct NUMBER *a){
 	int i = 0;
 
 	while(1){
@@ -77,36 +63,66 @@ int isZero(struct NUMBER *a){//ゼロかどうか
 		if((a->n[i] != 0)) return -1;
 		i++;
 	}
-
 	return 1;
 }
 
-int mulBy10(struct NUMBER *a){//10倍
-	int i = KETA - 1;
-	int overflag = 0;
-	if (a->n[KETA-1] != 0) 
-		overflag = -1;
-	
-	while(1){
-		if(i <= 0) break;
-		a->n[i] = a->n[i-1];
-		i--;
-	}
-	a->n[0] = 0;
-
-	return overflag;
+//桁数を受け取る
+int getKeta(struct NUMBER *a){
+	int i = KETA;
+	while(a->n[i-1] == 0) i--;
+	return i;
 }
 
-int divBy10(struct NUMBER *a){
+//10倍
+void mulBy10(struct NUMBER *a){
+	int i = KETA - 1;
+	int carry = 0;
+
+	a->n[KETA-1] = a->n[KETA-1] % (KISU / 10);
+	a->n[KETA-1] *= 10;
+	
+	while(1){
+		if(i < 0) break;
+		carry = a->n[i-1] / (KISU / 10);
+		a->n[i] += carry;
+		a->n[i-1] %= (KISU / 10);
+		a->n[i-1] *= 10;
+		i--;
+	}
+}
+
+//10分の1
+void divBy10(struct NUMBER *a){
 	int i = 0;
 
 	while(1){
 		if(i > KETA-2) break;
-		a->n[i] = a->n[i+1];
+		a->n[i] /= 10;
+		a->n[i] += (a->n[i+1] % 10) * (KISU / 10);
 		i++;
 	}
-	a->n[KETA-1] = 0;
-	return 0;
+	a->n[KETA-1] /= 10;
+}
+
+//1桁での割り算
+int divby1keta(struct NUMBER *a, struct NUMBER *b, int d){
+	int h, i, ai, t;
+
+	clearByZero(b);
+
+	i = getKeta(a) - 1;
+	h = 0;
+
+	while(1){
+		if(i < 0) break;
+
+		ai = a->n[i];
+		t = KISU * h + ai;
+		h = t % d;
+		b->n[i] = (t - h) / d;
+		i--;
+	}
+	return h;
 }
 
 int setInt(struct NUMBER *a, int x){
@@ -116,9 +132,9 @@ int setInt(struct NUMBER *a, int x){
 
 	while(1){
 		if(abs(x) <= 0) break;
-		amari = x%10;
+		amari = x % KISU;
 		a->n[i] = abs(amari);
-		x /= 10;
+		x /= KISU;
 		i++;
 	}
 	return 0;
@@ -136,12 +152,6 @@ int numComp(struct NUMBER *a , struct NUMBER *b){
 	return 0;
 }
 
-int getKeta(struct NUMBER *a){
-	int i = KETA;
-	while(a->n[i-1] == 0) i--;
-	return i;
-}
-
 int add(struct NUMBER *a, struct NUMBER *b, struct NUMBER *c){
 	int d;
 	int e = 0;
@@ -151,9 +161,9 @@ int add(struct NUMBER *a, struct NUMBER *b, struct NUMBER *c){
 	while(1){
 		if(i > KETA-1) break;
 		d = a->n[i] + b->n[i] + e;
-		c->n[i] = d % 10;
-		d /= 10;
-		e = d % 10;
+		c->n[i] = d % KISU;
+		d /= KISU;
+		e = d % KISU;
 		i++;
 	}
 	if(d > 0) ret = -1;
@@ -166,27 +176,23 @@ int sub(struct NUMBER *a, struct NUMBER *b, struct NUMBER *c){
 	int ai,bi;
 	int i = 0;
 	clearByZero(c);
-	if(numComp(a,b) > -1){
-		while(1){
-			if(i > KETA-1) break;
-			ai = a->n[i];
-			bi = b->n[i];
-			ai -= h;
-			if(ai >= bi){
-				c->n[i] = ai - bi;
-				h = 0;
-			} 
-			if(ai < bi){
-				c->n[i] = 10 + ai - bi;
-				h = 1;
-			}
-			i++;
+
+	while(1){
+		if(i > KETA-1) break;
+		ai = a->n[i];
+		bi = b->n[i];
+		ai -= h;
+		if(ai >= bi){
+			c->n[i] = ai - bi;
+			h = 0;
+		} 
+		if(ai < bi){
+			c->n[i] = KISU + ai - bi;
+			h = 1;
 		}
+		i++;
 	}
-	else{
-		sub(b,a,c);
-		setSign(c,-1);
-	}
+	
 	return 0;
 }
 
@@ -212,97 +218,51 @@ int decrement(struct NUMBER *a, struct NUMBER *b){
 	return r;
 }
 
-int simpleMultiple(int a, int b, int *c){
-	int i = 1;
-	*c = 0;
-
-	while(1){
-		if(i > b) break;
-		*c += a;
-		i++;
-	}
-	
-	return 0;
-}
-
-int multiple(struct NUMBER *a, struct NUMBER *b, struct NUMBER *c){
-	int bi,aj;
-	int i = 0;
-	int j = 0;
-	int h = 0;
-	struct NUMBER d;
-	clearByZero(c);
-
-		int e;
-		while(1){
-			if(i > KETA-1) break;
-			bi = b->n[i];
-			j = 0;
-			h = 0;
-
-			clearByZero(&d);
-
-			while(1){
-				if (j > KETA - i) break;
-
-				aj = a->n[j];
-				simpleMultiple(aj,bi,&e);
-				e += h;
-				d.n[j+i] = e % 10;
-				h = e / 10;
-				if((j == (KETA - 1)) && h != 0) return -1;
-				j++;
-			}
-			add(c,&d,c);
-			i++;
-	}
-	return 0;
-}
-
-
-
 int divide(struct NUMBER *a, struct NUMBER *b, struct NUMBER *c, struct NUMBER *d){
-	struct NUMBER e,f;
+	struct NUMBER e,f,a2;
 
+	clearByZero(&a2);
+	copyNumber(a,&a2);
 	clearByZero(c);
 
-	while(1){
-		if(numComp(a, b) >= 0){
-			copyNumber(b, d);
-			clearByZero(&e);
-			setInt(&e, 1);
+	if(getKeta(b) == 1){
+		d->n[0] = divby1keta(&a2, c, b->n[0]);
+	}
+	else{
+		while(1){
+			if(numComp(&a2, b) >= 0){
+				copyNumber(b, d);
+				clearByZero(&e);
+				setInt(&e, 1);
 
-			while(1){
-				if(numComp(a, d) < 1) break;
-				mulBy10(d);
-				mulBy10(&e);
+				while(1){
+					if(numComp(&a2, d) < 1) break;
+					mulBy10(d);
+					mulBy10(&e);
+				}
+
+				if(numComp(b, d) != 0){
+					divBy10(d);
+					divBy10(&e);
+				}
+
+				sub(&a2, d, &f);
+
+				copyNumber(&f, &a2);
+				add(c, &e, c);
+			} else {
+				copyNumber(&a2, d);
+				break;
 			}
-			divBy10(d);
-			divBy10(&e);
-
-			sub(a, d, &f);
-			copyNumber(&f, a);
-			add(c, &e, c);
-		} else {
-			copyNumber(a, d);
-			break;
 		}
 	}
-
 	return 0;
 }
 
 void sqrt_newton(struct NUMBER *N, struct NUMBER *x){
-	struct NUMBER b,c,d,e,f;
-	clearByZero(&b);
-	clearByZero(&c);
-	clearByZero(&e);//2
-	clearByZero(&f);
+	struct NUMBER b, c, d;
 
-	setInt(&e,2);
-
-	divide(N, &e, x, &c);
-	
+	divby1keta(N, x, 2);
 	copyNumber(x, &b);
 	copyNumber(x, &c);
 
@@ -311,44 +271,52 @@ void sqrt_newton(struct NUMBER *N, struct NUMBER *x){
 		copyNumber(x, &b);
 		divide(N, &b, x, &d);
 		add(x, &b, x);
-		divide(x, &e, &f, &d);
-		copyNumber(&f,x);
+		divby1keta(x, &d, 2);
+		copyNumber(&d, x);
 
 		if(numComp(x, &b) == 0) break;
-
 		if(numComp(x, &c) == 0){
 			if(numComp(x, &b)) copyNumber(&b, x);
 			break;
 		}
-	}
+	} 
 }
 
 void soinsu(struct NUMBER *x, struct NUMBER *ans){
-	struct NUMBER root_x, num, a, b, c;
+	struct NUMBER root_x, num, a, b, x2;
 	clearByZero(&root_x);
 	clearByZero(&num);
 	clearByZero(&a);
 	clearByZero(&b);
-	copyNumber(x, &c);
+	copyNumber(x, &x2);
 	setInt(&num, 2);
-	sqrt_newton(x, &root_x);
+	sqrt_newton(&x2, &root_x);
 
-	while(numComp(&c, &root_x) == 1 && numComp(&c, &num) == 1){
-		divide(&c, &num, &a, &b);
+	//2で割れるだけ割る
+	while(1){
+		if((x2.n[0] % 2) == 1)break;
+
+		divby1keta(&x2, &a, 2);
+		add(ans, &num, ans);
+		copyNumber(&a, &x2);
+	}
+	
+	increment(&num, &num);
+
+	while(numComp(&x2, &root_x) == 1 && numComp(&x2, &num) == 1){
+		divide(&x2, &num, &a, &b);
 		if(isZero(&b) == 1){
-			copyNumber(&a, &c);
 			add(ans, &num, ans);
-			divide(&c, &num, &a, &b);
+			copyNumber(&a, &x2);
 		}
 		else{
+			//3->5->7...のように偶数を飛ばして割る数を加算
+			increment(&num, &num);
 			increment(&num, &num);
 		}
 	}
-	decrement(&c, &a);
-
-	if(isZero(&a) != 1){
-		add(ans, &c, ans);
-	}
+	
+	if(x2.n[0] != 1 || getKeta(&x2) > 1) add(ans, &x2, ans);
 }
 
 void ruthaaron(struct NUMBER *a, int roop_times){
@@ -373,7 +341,8 @@ void ruthaaron(struct NUMBER *a, int roop_times){
 			dispNumber(a);
 			printf("\n");
 			dispNumber(&num);
-			printf("\n\n");
+			printf("\n");
+			printf("\n");
 		}
 
 		copyNumber(&current, &before);
@@ -385,37 +354,61 @@ void ruthaaron(struct NUMBER *a, int roop_times){
 }
 
 int main (int argc,char **argv){
+	struct timeval tv;
 	srandom(time(NULL));
 
-	struct NUMBER a,b,c,d;
+	double tstart, tend;
+	struct NUMBER a, b, c,d;
 	clearByZero(&a);
 	clearByZero(&b);
 	clearByZero(&c);
 	clearByZero(&d);
 
-	setRnd(&a, 2);
-	setRnd(&b, 2);
-	//setInt(&a,2);
-	setSign(&a, 1);
-	setSign(&b, 1);
-
-	//printf("start number is\n");
+	//setRnd(&a, KETA-1);
+	setInt(&a, 2);
+	printf("start number is\n");
 	dispNumber(&a);
 	printf("\n");
-	dispNumber(&b);
-	printf("\n\n");
+	
+	//ruthaaron(&a,3);
 
-	multiple(&a, &b, &c);
+	gettimeofday(&tv, NULL);
+	tstart = (double)tv.tv_sec + (double)tv.tv_usec * 1.e-6;
 
-	//divide(&a,&b,&c,&d);
+	for(int i = 0; i< 100; i++){
+		clearByZero(&a);
+		clearByZero(&b);
+		clearByZero(&c);
+		clearByZero(&d);
+		setRnd(&a,100);
+		setRnd(&b,random() % 100 + 1);
 
-	//ruthaaron(&a,1000000000);
+		dispNumber(&a);
+		printf("\n");
+		dispNumber(&b);
+		printf("\n");
+
+		divide(&a,&b,&c,&d);
+
+		dispNumber(&c);
+		printf("\n");
+		dispNumber(&d);
+		printf("\n");
+		printf("\n");
+
+	}
+	//ruthaaron(&a, 1000);
+
+	tend = (double)tv.tv_sec + (double)tv.tv_usec * 1.e-6;
 
 	//printf("end number is\n");
 	//decrement(&a, &b);
-	dispNumber(&c);
-	printf("\n");
-	//dispNumber(&d);
+
+	//dispNumber(&b);
+	//printf("\n");
+
+	printf("所要時間 = %f秒\n", tend - tstart);
+	
 
 	return 0;
 }
